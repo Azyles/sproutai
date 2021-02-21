557927ea542bf58c15fabb5eb59692b596322452
Pranav Krishna <krishnatechpranav@gmail.com>
2021-02-20 19:57:34 -0800

    asdf

--- lib/addGardenScreen.dart
+++ lib/addGardenScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddGarden extends StatefulWidget {
@@ -22,48 +23,171 @@ class _AddGardenState extends State<AddGarden> {
           )),
           child: Column(
             children: [
-              Container(
-                  height: MediaQuery.of(context).size.height * 0.70,
-                  margin: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.25),
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
+              ClipRect(
+                child: new BackdropFilter(
+                  filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: new Container(
+                      height: MediaQuery.of(context).size.height * 0.70,
+                      margin: EdgeInsets.only(
+                          top: MediaQuery.of(context).size.height * 0.25),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey[600].withOpacity(0.4)),
+                      child: Column(
                         children: [
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 25, top: 20),
+                                child: Text(
+                                  "Add Garden",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 25, top: 20),
+                                child: Text(
+                                  "Garden Name",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color.fromRGBO(184, 184, 184, 1)),
+                                ),
+                              ),
+                            ],
+                          ),
+                          ClipRect(
+                              child: new BackdropFilter(
+                                  filter: new ImageFilter.blur(
+                                      sigmaX: 20.0, sigmaY: 20.0),
+                                  child: new Container(
+                                    child: Container(
+                                      margin: EdgeInsets.only(top: 10),
+                                      height: 40,
+                                      padding:
+                                          EdgeInsets.only(left: 10, bottom: 5),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.80,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white.withOpacity(0.15),
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: TextField(
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ))),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 25, top: 20),
+                                child: Text(
+                                  "State/Region",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color.fromRGBO(184, 184, 184, 1)),
+                                ),
+                              ),
+                            ],
+                          ),
+                          ClipRect(
+                              child: new BackdropFilter(
+                                  filter: new ImageFilter.blur(
+                                      sigmaX: 20.0, sigmaY: 20.0),
+                                  child: new Container(
+                                    child: Container(
+                                      margin: EdgeInsets.only(top: 10),
+                                      height: 40,
+                                      padding:
+                                          EdgeInsets.only(left: 10, bottom: 5),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.80,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white.withOpacity(0.15),
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: TextField(
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ))),
+                          Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 25, top: 20),
+                                child: Text(
+                                  "Country",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Color.fromRGBO(184, 184, 184, 1)),
+                                ),
+                              ),
+                            ],
+                          ),
+                          ClipRect(
+                              child: new BackdropFilter(
+                                  filter: new ImageFilter.blur(
+                                      sigmaX: 20.0, sigmaY: 20.0),
+                                  child: new Container(
+                                    child: Container(
+                                      margin: EdgeInsets.only(top: 10),
+                                      height: 40,
+                                      padding:
+                                          EdgeInsets.only(left: 10, bottom: 5),
+                                      width: MediaQuery.of(context).size.width *
+                                          0.80,
+                                      decoration: BoxDecoration(
+                                          color: Colors.white.withOpacity(0.15),
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: TextField(
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ))),
                           Container(
-                            margin: EdgeInsets.only(left: 25, ),
-                            child: Text(
-                              "Add Garden",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 25),
+                            height: 45,
+                            width: MediaQuery.of(context).size.width * 0.80,
+                            margin: EdgeInsets.only(
+                                top: MediaQuery.of(context).size.height * 0.22),
+                            child: RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              color: Color.fromRGBO(64, 206, 162, 1),
+                              onPressed: () {
+                                FirebaseFirestore.instance
+                                    .collection("Gardens")
+                                    .doc()
+                                    .set({
+                                      "name": 
+                                    });
+                              },
+                              child: Text(
+                                "Add Garden",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
                             ),
                           ),
                         ],
-                      ),
-                      Container(
-                        height: 45,
-                        width: MediaQuery.of(context).size.width * 0.80,
-                        child: RaisedButton(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          color: Color.fromRGBO(64, 206, 162, 1),
-                          onPressed: () {},
-                          child: Text(
-                            "Add Garden",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
+                      )),
+                ),
+              ),
             ],
           ),
         ),
